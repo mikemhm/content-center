84e63586795a616e106f891af1649ecf75ca0fa9
mike <mikemhm@126.com>
2020-07-29 20:21:35 +0800

    添加 登录检查

---
+++ src/main/java/com/itmuch/contentcenter/security/SecurityException.java
@@ -0,0 +1,14 @@
+package com.itmuch.contentcenter.security;
+
+
+/**
+ * @author mike
+ */
+public class SecurityException extends RuntimeException {
+    public SecurityException() {
+        super();
+    }
+    public SecurityException(String message) {
+        super(message);
+    }
+}
