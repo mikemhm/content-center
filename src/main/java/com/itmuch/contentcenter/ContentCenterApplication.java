e8eace74ad9cdd8e4e442c526c2b4fe044f43f76
mike <mikemhm@126.com>
2020-09-09 10:07:14 +0800

    切换版本，配合RocketMQ兼容

--- src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
+++ src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
+
 @MapperScan("com.itmuch")
 @SpringBootApplication
 @EnableFeignClients
